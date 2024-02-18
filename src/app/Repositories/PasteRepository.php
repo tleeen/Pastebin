08cd625f434f01ca9831930ff41e82a89c3d1280
tleeen <peterivan5555@gamil.com>
2024-02-18 15:41:48 +0700

    fix(repositories):refactor roles for getting pastes by id

--- src/app/Repositories/PasteRepository.php
+++ src/app/Repositories/PasteRepository.php
@@ -44,7 +44,22 @@ class PasteRepository implements PasteRepositoryInterface
      */
     public function getById(string $id): Paste
     {
-        return $this::rules()
+        $userId = (auth()->user()) ? auth()->user()->id : 0;
+
+        return Paste::join('expiration_times', 'pastes.expiration_time_id', '=', 'expiration_times.id')
+            ->where('pastes.created_at', '>', DB::raw('now() - INTERVAL expiration_times.volume MINUTE - INTERVAL 7 HOUR'))
+            ->select('pastes.*')
+            ->where(function ($query) use ($userId){
+                $query->whereHas('access_modifier', function ($query) use ($userId){
+                    $query->whereIn('title', ['public', 'unlisted']);})
+                    ->orWhere('author_id', $userId)
+                    ->where(function ($query){
+                        $query->WhereHas('access_modifier', function ($query) {
+                            $query->where('title', 'private');})
+                            ->orWhereHas('access_modifier', function ($query) {
+                                $query->where('title', 'unlisted ');});
+                    });
+            })
             ->find($id);
     }
 
@@ -79,7 +94,7 @@ class PasteRepository implements PasteRepositoryInterface
 
     public static function rules(): Builder
     {
-        $userId = (auth()->user()) ? auth()->user()->id : null;
+        $userId = (auth()->user()) ? auth()->user()->id : 0;
 
         return Paste::join('expiration_times', 'pastes.expiration_time_id', '=', 'expiration_times.id')
             ->where('pastes.created_at', '>', DB::raw('now() - INTERVAL expiration_times.volume MINUTE - INTERVAL 7 HOUR'))
