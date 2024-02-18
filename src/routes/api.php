c9168fdc38844ac5e67d025a2b54631b6c38f06b
tleeen <peterivan5555@gamil.com>
2024-02-19 01:07:35 +0700

    feat(api): creating routes for open api

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,22 +17,61 @@ Route::prefix('/v1')
     ->group(function (){
         Route::prefix('/pastes')
             ->group(function (){
-
+                Route::middleware('jwt.auth')
+                    ->middleware('admin.api')
+                    ->get('/', [PasteController::class, 'index']);
+                Route::middleware('jwt.auth')
+                    ->middleware('admin.api')
+                    ->delete('/{id}', [PasteController::class, 'destroy']);
+                Route::post('/', [PasteController::class, 'store']);
+                Route::get('/all', [PasteController::class, 'getAll']);
+                Route::get('/{id}', [PasteController::class, 'getById']);
+                Route::get('/latest', [PasteController::class, 'latest']);
             });
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('user', [AuthController::class, 'user']);
-});
+        Route::prefix('/users')
+            ->group(function (){
+                Route::middleware('jwt.auth')
+                    ->middleware('admin.api')
+                    ->get('/', [UserController::class, 'index']);
+                Route::middleware('jwt.auth')
+                    ->middleware('admin.api')
+                    ->delete('/{id}', [UserController::class, 'destroy']);
+                Route::prefix('pastes')
+                    ->group(function (){
+                        Route::get('/', [UserController::class, 'getPastes']);
+                        Route::get('/latest', [UserController::class, 'getLastPastes']);
+                    });
+            });
+
+        Route::prefix('/complaints')
+            ->group(function (){
+                Route::middleware('jwt.auth')
+                    ->middleware('admin.api')
+                    ->get('/', [ComplaintController::class, 'index']);
+                Route::post('/', [ComplaintController::class, 'store']);
+            });
+
+        Route::get('/types', [TypeController::class, 'index']);
+
+        Route::get('/access_modifiers', [AccessModifierController::class, 'index']);
+
+        Route::get('/expiration_times', [ExpirationTimeController::class, 'index']);
+
+        Route::prefix('auth')
+            ->middleware('api')
+            ->group(function (){
+                Route::post('login', [AuthController::class, 'login']);
+                Route::post('logout', [AuthController::class, 'logout']);
+                Route::post('refresh', [AuthController::class, 'refresh']);
+                Route::post('user', [AuthController::class, 'user']);
+            });
+
+    });
 
 
 //Route::middleware('jwt.auth')
 //    ->middleware('admin.api')
 //    ->get('/pastes', [\App\Repositories\PasteRepository::class, 'getAll']);
-});
+
 
